f283ba7219f8b4513383261e7d7e3f635f444548
milad <miladarabali1@gmail.com>
2023-02-22 22:45:57 +0330

    finish game

--- javascript.js
+++ javascript.js
@@ -2,12 +2,14 @@ const secrentphrases =["milad","you","dad","hello","break"];
 let random_item = " ";
 let clicked = [];
 let result ="";
+let mistake = 0 ;
 
 // select random name
 function selectrandomitem (){
     random_item = secrentphrases[Math.floor(Math.random()* secrentphrases.length)];
     console.log(random_item)
     document.getElementById("container").addEventListener("click",inputphrases)
+    window.addEventListener("keydown",keyhander)
 }
 function setunderscorse (){
     let splitword = random_item.split("");
@@ -23,13 +25,22 @@ function checkwin (){
         document.getElementById("Gameover").querySelector("p").style.display="block";
         document.getElementById("image").querySelector("img").src="./assets/winner.png";
     }
-
-
-
-
-
-
 }
+//user mistake
+function userMistake (){
+    if(mistake === 6){
+        document.getElementById("Gameover").querySelector("p").style.display="block";
+        document.getElementById("clue").querySelector("p").innerHTML=`<p>word is : ${random_item}</p>`
+    }
+}
+function updateimagemistake (){
+    const img = document.getElementById("image").querySelector("img")
+    img.src = `./assets/hangman${mistake}.png`
+}
+
+
+
+
 // promp from phrases container
 function contaierPhrases(container){
     container = container.toLowerCase();
@@ -39,7 +50,9 @@ function contaierPhrases(container){
         setunderscorse();
         checkwin ();
     }else if(random_item.indexOf(container) === -1){
-
+        mistake++;
+        userMistake ();
+        updateimagemistake ();
     }
 
 
@@ -49,6 +62,11 @@ function contaierPhrases(container){
 //  input from mouse and Keyboard 
 function inputphrases (l) {
     contaierPhrases(l.target.id)
+    // console.log(l.target.id)
+
+}
+function keyhander (l) {
+    contaierPhrases(l.key)
     // console.log(l.target.id)
 
 }
